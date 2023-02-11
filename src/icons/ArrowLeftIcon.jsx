0a966ca236e5011f58004cccc70dd9c09a29cc9d
gogaevalex <gogaevalex@gmail.com>
2023-02-11 16:18:22 +0300

    2 page

--- src/icons/ArrowLeftIcon.jsx
+++ src/icons/ArrowLeftIcon.jsx
@@ -1,7 +1,7 @@
 export const ArrowLeftIcon = () => {
     return (
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M10 13L6 8L10 3" stroke={"var(--tg-theme-text-color)"} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M11.7647 15.2941L7.05884 9.41177L11.7647 3.52942" stroke={"var(--tg-theme-text-color)"} stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
     )
 };
