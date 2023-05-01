d875a27badf40361066f0bbd530e9b69b9a28586
Johny <stasinbox@inbox.ru>
2023-05-02 01:08:52 +0200

    no initializeData install cookies

--- src/utils/api.js
+++ src/utils/api.js
@@ -8,16 +8,10 @@ const $api = axios.create({
     baseURL: API_URL
 })
 
+
 $api.interceptors.request.use((config) => {
-    const token = Cookies.get('jwt');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
+    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
     return config;
-});
-// $api.interceptors.request.use((config) => {
-//     config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
-//     return config;
-// })
+})
 
 export default $api;
